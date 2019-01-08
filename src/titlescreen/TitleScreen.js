5f0ccaa88da4aea4b9d69e3c53c666efa6f782b7
Kennith Chan <kennith.chan@videri.com>
2019-01-07 20:29:21 -0500

    Changed stage to stagegl, uses caches

--- src/titlescreen/TitleScreen.js
+++ src/titlescreen/TitleScreen.js
@@ -6,16 +6,16 @@ class TitleScreen extends GameContext {
         super(controller);
         console.log("TitleScreen(controller)");
         this.titleText = new createjs.Text("Room Tester", "40px Arial", "#3010FF");
-        this.titleText.x = this.stage.canvas.width/2;
-        this.titleText.y = 30;
         this.titleText.textAlign = "center";
 
-        let btnCenter = [this.stage.canvas.width/2,200];
         let btnDim = [200,100];
         this.buttonBg = new createjs.Shape();
         this.buttonBg.graphics.beginFill("#80A080").drawRoundRect(
-            btnCenter[0] - btnDim[0]/2,btnCenter[1] - btnDim[1]/2,
+            btnDim[0]/-2,btnDim[1]/-2,
             btnDim[0],btnDim[1],10);
+        this.buttonBg.cache(
+            btnDim[0]/-2,btnDim[1]/-2,
+            btnDim[0],btnDim[1]);
         this.buttonBg.addEventListener("click",((event) =>
             {
                 console.log("Clicked at "+event.stageX+","+event.stageY);
@@ -25,15 +25,16 @@ class TitleScreen extends GameContext {
             }).bind(this));
 
         this.goButtonText = new createjs.Text("GO", "40px Arial", "#3010FF");
-        this.goButtonText.x = btnCenter[0];
-        this.goButtonText.y = btnCenter[1]-20;
         this.goButtonText.textAlign = "center";
-        this.goButtonText.visible = false;
+        let goBtnBound = this.goButtonText.getBounds();
+        this.goButtonText.cache(goBtnBound.x,goBtnBound.y,
+            goBtnBound.width,goBtnBound.height);
 
         this.loadButtonText = new createjs.Text("LOADING", "20px Arial", "#3010FF");
-        this.loadButtonText.x = btnCenter[0];
-        this.loadButtonText.y = btnCenter[1]-20;
         this.loadButtonText.textAlign = "center";
+        let loadBtnBound = this.loadButtonText.getBounds();
+        this.loadButtonText.cache(loadBtnBound.x,loadBtnBound.y,
+            loadBtnBound.width,loadBtnBound.height);
         
         this.doneLoading = false;
     }
@@ -46,9 +47,23 @@ class TitleScreen extends GameContext {
         this.gameCore.assetHandler.preloadAssets();
 
         this.stage.addChild(this.buttonBg);
+        let btnCenter = [this.stage.canvas.width/2,200];
+        this.buttonBg.x = btnCenter[0];
+        this.buttonBg.y = btnCenter[1];
+
         this.stage.addChild(this.titleText);
+        this.titleText.x = this.stage.canvas.width/2;
+        this.titleText.y = 30;
+        
         this.stage.addChild(this.goButtonText);
+        this.goButtonText.x = btnCenter[0];
+        this.goButtonText.y = btnCenter[1]-20;
+        this.goButtonText.visible = false;
+
         this.stage.addChild(this.loadButtonText);
+        this.loadButtonText.x = btnCenter[0];
+        this.loadButtonText.y = btnCenter[1]-20;
+        
         this.stage.update();
     }
 
